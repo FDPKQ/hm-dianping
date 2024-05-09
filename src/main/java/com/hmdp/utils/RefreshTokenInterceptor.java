3276db244c4a4d94b26c77b8f52c436f86d570c5
luoxianjun <luoxianjun@cumt.edu.cn>
2024-05-09 10:18:01 +0800

    p34

--- src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
+++ src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
@@ -3,17 +3,11 @@ package com.hmdp.utils;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.UserDTO;
-import com.hmdp.entity.User;
-import io.netty.util.internal.StringUtil;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -21,14 +15,28 @@ import static com.hmdp.utils.RedisConstants.LOGIN_USER_KEY;
 import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
 
 
-public class LoginInterceptor implements HandlerInterceptor {
-    
+public class RefreshTokenInterceptor implements HandlerInterceptor {
+
+    StringRedisTemplate stringRedisTemplate;
+
+    public RefreshTokenInterceptor(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (UserHolder.getUser() == null) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return false;
-        }
+        String token = request.getHeader("authorization");
+        if (StrUtil.isBlank(token)) return true;
+
+        Map<Object, Object> userMap = stringRedisTemplate.opsForHash()
+                .entries(LOGIN_USER_KEY + token);
+        if (userMap.isEmpty()) return true;
+
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+
+        UserHolder.saveUser(userDTO);
+
+        stringRedisTemplate.expire(LOGIN_USER_KEY + token, LOGIN_USER_TTL, TimeUnit.SECONDS);
 
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
